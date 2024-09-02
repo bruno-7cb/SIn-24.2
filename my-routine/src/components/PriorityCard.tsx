a03c1fdb48485de41b1b0bd3b7db0181c0815849
bruno-7cb <163890780+bruno-7cb@users.noreply.github.com>
2024-09-02 22:06:37 +0000

    atualizacao do tailwind

--- my-routine/src/components/PriorityCard.tsx
+++ my-routine/src/components/PriorityCard.tsx
@@ -2,34 +2,40 @@ import { useState } from 'react';
 
 export default function PriorityCard() {
   // Estado para armazenar a lista de itens
-  const [items, setItems] = useState<string[]>(['um', 'doi']);
+  const [items, setItems] = useState<string[]>(['um', 'dois']);
+  const [newItem, setNewItem] = useState<string>('');
 
   // Função para adicionar um novo item à lista
-  const addItem = () => setItems((prevItems) => [...prevItems, 'Novo Item']);
+  const addItem = () => {
+    if (newItem.trim() !== '') {
+      setItems((prevItems) => [...prevItems, newItem]);
+      setNewItem('');
+    }
+  }
 
-  // Função para lidar com mudanças nos itens editáveis
-  const handleBlur = (index: number, event: React.FocusEvent<HTMLLIElement>) => {
+  const handleInput = (index: number, value: string) => {
     const updatedItems = [...items];
-    updatedItems[index] = event.currentTarget.innerText;
-    setItems(updatedItems);
+    updatedItems[index] = value;
+    return updatedItems;
   };
 
   return (
     <section>
       <h2>Priorities</h2>
-      <button type="button" onClick={addItem}>+</button>
       <ol>
         {items.map((item, index) => (
           <li
             key={index}
             contentEditable
             suppressContentEditableWarning={true}
-            onBlur={(event) => handleBlur(index, event)}
+            onBlur={(e) => handleInput(index, e.currentTarget.innerText)}
           >
             {item}
           </li>
         ))}
       </ol>
+      <input type='text' value={newItem} onChange={(e) => setNewItem(e.target.value)} placeholder='Novo Item'/>
+      <button type="button" onClick={addItem}>Adicionar Items</button>
     </section>
   );
 }
--- my-routine/src/pages/Planner.tsx
+++ my-routine/src/pages/Planner.tsx
@@ -6,7 +6,7 @@ import Registration from "../components/Registration";
 
 export default function Planner() {
     return (
-        <main className="bg-slate-300 w-1/2 mx-auto max-w-90 p-6">
+        <main className="container bg-slate-300 mx-auto px-4 py-6 columns-2 gap-6">
             <Header />
             <Registration />
             <CardList title="morning" />
